bebc62d50f888dd93eb5b02c4504eeef25e470c3
Daniel Holmberg <danielkurtholmberg@gmail.com>
2016-05-10 16:09:18 +0200

    Uppdaterade vilka som är med i projektet högst upp

--- src/MainGame/Main.java
+++ src/MainGame/Main.java
@@ -6,7 +6,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 /**
- * Created by Victor on 2016-05-10.
+ * Created by Victor, Daniel, Henrik, Linnea, David, Erik on 2016-05-10.
  */
 public class Main {
 
