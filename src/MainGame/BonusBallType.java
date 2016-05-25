72671607cad40c66493cea93ea1fd7d84e754633
Scypier <Combler94@gmail.com>
2016-05-25 14:10:09 +0200

    Added BonusBallType

---
+++ src/MainGame/BonusBallType.java
@@ -0,0 +1,48 @@
+package MainGame;
+
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * Created by David on 2016-05-25.
+ */
+public enum BonusBallType {
+
+    SLOW (Color.GREEN, 2.5),
+    MEDIUM(Color.BLUE, 5),
+    FAST(Color.RED, 7.5),
+    VERY_FAST(Color.BLACK, 10);
+
+    private final Color color;
+    private final double loadedVelocity;
+
+    private BonusBallType(Color color, double yVelocity) {
+        this.color = color;
+        this.loadedVelocity = yVelocity;
+    }
+
+    public static BonusBallType getRandomBallType() {
+        Random rng = new Random();
+        int randomNumber = rng.nextInt(4);
+        switch(randomNumber) {
+            case 0:
+                return SLOW;
+            case 1:
+                return MEDIUM;
+            case 2:
+                return FAST;
+            case 3:
+                return VERY_FAST;
+            default:
+                return MEDIUM;
+        }
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public double getLoadedVelocity() {
+        return loadedVelocity;
+    }
+}
